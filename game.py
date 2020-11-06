ad6af7a9d92f88583d7cd38a7e2b5992d1b72fdb
Raul Faife <35008453+RaulFaife@users.noreply.github.com>
2020-11-05 20:10:13 -0500

    f

---
+++ game.py
@@ -0,0 +1,22 @@
+#imports
+import time
+
+
+#Introduction
+print("Welcome to Hunters Wind \n I created this game as a tribute to my old pet guinea pig hunter \n hope you enjoy :) \n \n")
+
+begin = input("Type the letter Y to begin: ")
+while begin != "Y":
+    begin = input("Type the letter Y to begin: ")
+
+#Chapter 1 - Home Sweet Home
+print("Chapter 1: Home Sweet Home")
+time.sleep(2)
+print("You Wake up getting a fresh whif of cheetos and sweat")
+time.sleep(2)
+print("You remember you are inside your owner's hoodie and you begin to loosen up")
+time.sleep(2)
+print("You begin thinking to yourself where you are heading")
+time.sleep(2)
+print("You hear a more high pitch voice and a very low sounding voice speaking to your owner")
+time.sleep(2)
